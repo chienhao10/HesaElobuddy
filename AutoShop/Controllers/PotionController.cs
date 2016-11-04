d09d58d50e53c45f23ddfee5e823e036bc2041fa
Sylvain Martens <thehesa@gmail.com>
2016-11-04 19:53:04 -0400

    Updated Potion Selling to also use random timing between sell

--- AutoShop/Controllers/PotionController.cs
+++ AutoShop/Controllers/PotionController.cs
@@ -9,27 +9,28 @@ namespace AutoShop.Controllers
         static int randomNumber = new Random().Next(75, 125);
         public static void BuyOrSellPotions()
         {
-            if (BuildController.CurrentBuild == null || !BuildController.CurrentBuild.UseHPotion) return;
-            if(ObjectManager.Player.Level <= BuildController.CurrentBuild.MaxHPotionLevel)
+            if (count == 0)
             {
-                //Buy Healing Potions
-                if (ItemController.HPotionCount() >= BuildController.CurrentBuild.MaxHPotionCount) return;
-                if(count == 0)
+                randomNumber = new Random().Next(75, 125);
+                //
+                if (BuildController.CurrentBuild == null || !BuildController.CurrentBuild.UseHPotion) return;
+                if (ObjectManager.Player.Level <= BuildController.CurrentBuild.MaxHPotionLevel)
                 {
+                    //Buy Healing Potions
+                    if (ItemController.HPotionCount() >= BuildController.CurrentBuild.MaxHPotionCount) return;
                     ItemController.BuyHPotion();
-                    randomNumber = new Random().Next(75, 125);
                 }
-                count++;
-                if (count >= randomNumber) count = 0;
-            }
-            else
-            {
-                //Sell Healing Potions if any...
-                if (ItemController.HPotionCount() > 0)
+                else
                 {
-                    ItemController.SellHPotion();
+                    //Sell Healing Potions if any...
+                    if (ItemController.HPotionCount() > 0)
+                    {
+                        ItemController.SellHPotion();
+                    }
                 }
             }
+            count++;
+            if (count >= randomNumber) count = 0;
         }
     }
 }
