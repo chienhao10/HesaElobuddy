516f9d23d21a2bc37e04acbeddf281e6e9d63af5
Sylvain Martens <thehesa@gmail.com>
2016-11-17 22:58:22 -0500

    Simple Texture Disabler Addon

---
+++ TextureDisabler/TextureDisabler/Program.cs
@@ -0,0 +1,13 @@
+﻿using EloBuddy;
+
+namespace TextureDisabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hacks.DisableTextures = true;
+            ManagedTexture.OnLoad += delegate (OnLoadTextureEventArgs texture) { texture.Process = false; };
+        }
+    }
+}
